d2c47d184337699423e51b9fdd33d21dd012b98d
belaymit <2belamit@gmail.com>
2025-08-09 21:25:07 -0700

    feat: add dashboard UI components

---
+++ finance-dashboard/src/components/index.ts
@@ -0,0 +1,6 @@
+// Export all dashboard components
+export { DashboardHeader } from './DashboardHeader';
+export { FinancialSummary } from './FinancialSummary';
+export { ExchangeRates } from './ExchangeRates';
+export { Portfolio } from './Portfolio';
+export { RecentTransactions } from './RecentTransactions';
